eaa132d3e8d82f3bae79bbcc0f97a627db338be1
dell <dell@dell-PC>
2019-12-04 12:41:52 +0530

    intial commit

---
+++ src/DemoArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class DemoArray {
+
+	public static void main(String[] args) {
+		ArrayList l = new ArrayList();
+		List m =Collections.synchronizedList(l);
+		l.add("sekhar");
+		l.add("kiran");
+		l.add("raju");
+		System.out.println(l);
+		l.set(1, "SEKHAR");
+		System.out.println(l);
+		int a = l.indexOf("raju");
+		System.out.println(a);
+		int b = l.size();
+		System.out.println(b);
+		boolean c = l.contains("sekhar");
+		System.out.println(c);
+		System.out.println(m);
+	}
+
+};
